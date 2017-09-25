2c200d483818f74337695b78fa39ca72ace038f7
Claire Yan <silviaclaire@gmail.com>
2017-09-26 02:15:24 +0900

    All together in App component
    for test and reference

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { getCategories } from '../actions/category_actions'
+import { getPostsByCategory, getPosts, addPost, fetchPost, votePost, editPost, deletePost } from '../actions/post_actions'
+import { getComments, addComment, fetchComment, voteComment, editComment, deleteComment } from '../actions/comment_actions'
 import { sortPosts, sortComments } from '../actions/sort_actions'
 
 class App extends Component {
   componentDidMount() {
-    this.props.sortPosts()
-    this.props.sortPosts()
-    this.props.sortComments()
-    this.props.sortComments()
   }
   render() {
     return (
@@ -29,6 +28,20 @@ function mapStateToProps ({ categories, posts, comments, sort }) {
 
 function mapDispatchToProps (dispatch) {
   return {
+    getCategories: () => dispatch(getCategories()),
+    getPostsByCategory: (category) => dispatch(getPostsByCategory(category)),
+    getPosts: () => dispatch(getPosts()),
+    addPost: (body) => dispatch(addPost(body)),
+    fetchPost: (id) => dispatch(fetchPost(id)),
+    votePost: (id, option) => dispatch(votePost(id, option)),
+    editPost: (id, body) => dispatch(editPost(id, body)),
+    deletePost: (id) => dispatch(deletePost(id)),
+    getComments: (parentId) => dispatch(getComments(parentId)),
+    addComment: (body) => dispatch(addComment(body)),
+    fetchComment: (id) => dispatch(fetchComment(id)),
+    voteComment: (id, option) => dispatch(voteComment(id, option)),
+    editComment: (id, body) => dispatch(editComment(id, body)),
+    deleteComment: (id) => dispatch(deleteComment(id)),
     sortPosts: () => dispatch(sortPosts()),
     sortComments: () => dispatch(sortComments()),
   }
