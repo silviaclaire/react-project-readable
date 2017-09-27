758cb4cce21e705a5a1f42a837af759207ed4763
Claire Yan <silviaclaire@gmail.com>
2017-09-27 23:11:50 +0900

    Build displaying UI

--- src/components/DefaultView.js
+++ src/components/DefaultView.js
@@ -3,25 +3,81 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { getCategories } from '../actions/category_actions'
 import { getPostsByCategory, getPosts, addPost, fetchPost, votePost, editPost, deletePost } from '../actions/post_actions'
-import { getComments, addComment, fetchComment, voteComment, editComment, deleteComment } from '../actions/comment_actions'
-import { sortPosts, sortComments } from '../actions/sort_actions'
+import { getComments } from '../actions/comment_actions'
+import { sortPosts } from '../actions/sort_actions'
+import { capitalize } from '../utils/helpers'
+import sortBy from 'sort-by'
+import moment from 'moment'
+import { Nav, NavItem } from 'react-bootstrap'
+import EditIcon from 'react-icons/lib/fa/pencil'
+import DeleteIcon from 'react-icons/lib/fa/trash-o'
+import UpVoteIcon from 'react-icons/lib/fa/caret-up'
+import DownVoteIcon from 'react-icons/lib/fa/caret-down'
 
 class DefaultView extends Component {
+
+  componentDidMount() {
+    this.props.getCategories()
+    this.props.getPosts()
+  }
+
   render() {
+    console.log('props',this.props)
+    console.log('state',this.state)
+    const { currentCategory, categories, posts, comments, sort } = this.props
+
     return (
-      <div>
-        Default View
+      <div className='container'>
+
+        <h1>Readable</h1>
+
+        <Nav bsStyle='tabs' activeHref={ currentCategory === 'all' ? '/' : `/${currentCategory}`}>
+          <NavItem href='/'>All</NavItem>
+          {categories.map((category) => (
+            <NavItem key={category} href={`/${category}`}>{capitalize(category)}</NavItem>
+          ))}
+        </Nav>
+
+        <ul className='list'>
+          <div className='add-sort'>
+            <button>[ + ]</button>
+            <button>[ Sort by: {sort.posts} ]</button>
+          </div>
+          {posts.map((post) => (
+            <li key={post.id} className='list-item'>
+              <Link to={`/${post.category}/${post.id}`}><h4>{post.title}</h4></Link>
+              <div className='vote-edit-delete'>
+                <button><UpVoteIcon /></button>
+                {post.voteScore}
+                <button><DownVoteIcon /></button>
+                <button><EditIcon /></button>
+                <button><DeleteIcon /></button>
+              </div>
+              <div className='info'>
+                {moment(parseInt(post.timestamp,10)).calendar()}{', '}
+                author: {post.author}{', '}
+                comment: 0{', '}
+                category: {post.category}
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps ({ categories, posts, comments, sort }) {
+function mapStateToProps ({ categories, posts, comments, sort }, { match : { params : { category }}}) {
+  const currentCategory = category ? category : 'all'
+  const showingPosts = posts
+    .filter((post) => currentCategory === 'all' ? true : post.category === currentCategory)
+    .sort(sortBy(sort.posts))
   return {
-    categories,
-    posts,
-    comments,
-    sort
+    currentCategory: currentCategory,
+    categories: categories,
+    posts: showingPosts,
+    comments: comments,
+    sort: sort
   }
 }
 
@@ -36,13 +92,7 @@ function mapDispatchToProps (dispatch) {
     editPost: (id, body) => dispatch(editPost(id, body)),
     deletePost: (id) => dispatch(deletePost(id)),
     getComments: (parentId) => dispatch(getComments(parentId)),
-    addComment: (body) => dispatch(addComment(body)),
-    fetchComment: (id) => dispatch(fetchComment(id)),
-    voteComment: (id, option) => dispatch(voteComment(id, option)),
-    editComment: (id, body) => dispatch(editComment(id, body)),
-    deleteComment: (id) => dispatch(deleteComment(id)),
     sortPosts: () => dispatch(sortPosts()),
-    sortComments: () => dispatch(sortComments()),
   }
 }
 
--- src/components/PostView.js
+++ src/components/PostView.js
@@ -1,36 +1,93 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { getCategories } from '../actions/category_actions'
-import { getPostsByCategory, getPosts, addPost, fetchPost, votePost, editPost, deletePost } from '../actions/post_actions'
+import { fetchPost, votePost, editPost, deletePost } from '../actions/post_actions'
 import { getComments, addComment, fetchComment, voteComment, editComment, deleteComment } from '../actions/comment_actions'
-import { sortPosts, sortComments } from '../actions/sort_actions'
+import { sortComments } from '../actions/sort_actions'
+import sortBy from 'sort-by'
+import moment from 'moment'
+import HomeIcon from 'react-icons/lib/fa/home'
+import EditIcon from 'react-icons/lib/fa/pencil'
+import DeleteIcon from 'react-icons/lib/fa/trash-o'
+import UpVoteIcon from 'react-icons/lib/fa/caret-up'
+import DownVoteIcon from 'react-icons/lib/fa/caret-down'
 
 class PostView extends Component {
+  componentDidMount() {
+    const currentPostId = this.props.match.params.id
+    this.props.fetchPost(currentPostId)
+    this.props.getComments(currentPostId)
+  }
+
   render() {
+    console.log('props', this.props)
+    console.log('state',this.state)
+    const { post, comments, sort } = this.props
+
     return (
-      <div>
-        Post View
+      <div className='container'>
+
+        <Link to='/'><HomeIcon size={18}/></Link>
+
+        <div className='post-detail'>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+          <div className='vote-edit-delete'>
+            <button><UpVoteIcon /></button>
+            {post.voteScore}
+            <button><DownVoteIcon /></button>
+            <button><EditIcon /></button>
+            <button><DeleteIcon /></button>
+          </div>
+          <div className='info'>
+            {moment(parseInt(post.timestamp,10)).calendar()}{', '}
+            author: {post.author}{', '}
+            comments: {comments.length}{', '}
+            category: {post.category}
+          </div>
+        </div>
+
+        <ul className='list'>
+          <div className='comment-header'>
+            <h4>Comments ({comments.length})</h4>
+          </div>
+          <div className='add-sort'>
+            <button>[ + ]</button>
+            <button>[ Sort by: {sort.comments} ]</button>
+          </div>
+          {comments.map((comment) => (
+            <li key={comment.id} className='list-item'>
+              <div className='vote-edit-delete'>
+                <button><UpVoteIcon /></button>
+                {comment.voteScore}
+                <button><DownVoteIcon /></button>
+                <button><EditIcon /></button>
+                <button><DeleteIcon /></button>
+              </div>
+              <div className='info'>
+                {moment(parseInt(comment.timestamp,10)).calendar()}{', '}
+                author: {comment.author}{', '}
+              </div>
+              {comment.body}
+            </li>
+          ))}
+        </ul>
       </div>
     )
   }
 }
 
-function mapStateToProps ({ categories, posts, comments, sort }) {
+function mapStateToProps ({ posts, comments, sort }) {
+  const showingComments = comments.sort(sortBy(sort.comments))
   return {
-    categories,
-    posts,
-    comments,
-    sort
+    post: posts,
+    comments: showingComments,
+    sort: sort
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return {
-    getCategories: () => dispatch(getCategories()),
-    getPostsByCategory: (category) => dispatch(getPostsByCategory(category)),
-    getPosts: () => dispatch(getPosts()),
-    addPost: (body) => dispatch(addPost(body)),
     fetchPost: (id) => dispatch(fetchPost(id)),
     votePost: (id, option) => dispatch(votePost(id, option)),
     editPost: (id, body) => dispatch(editPost(id, body)),
@@ -41,7 +98,6 @@ function mapDispatchToProps (dispatch) {
     voteComment: (id, option) => dispatch(voteComment(id, option)),
     editComment: (id, body) => dispatch(editComment(id, body)),
     deleteComment: (id) => dispatch(deleteComment(id)),
-    sortPosts: () => dispatch(sortPosts()),
     sortComments: () => dispatch(sortComments()),
   }
 }
