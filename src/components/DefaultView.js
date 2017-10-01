a7f791700d7546b3bbfbbe64496c5b59c5e4f2f8
Claire Yan <silviaclaire@gmail.com>
2017-10-02 00:24:51 +0900

    Remove unnecessary actions in Views

--- src/components/DefaultView.js
+++ src/components/DefaultView.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 import { getCategories } from '../actions/category_actions'
-import { getPostsByCategory, getPosts, addPost, fetchPost, votePost, editPost, deletePost } from '../actions/post_actions'
-import { getComments } from '../actions/comment_actions'
+import { getPostsByCategory, getPosts, addPost } from '../actions/post_actions'
 import { sortPosts } from '../actions/sort_actions'
+import PostContainer from './PostContainer'
 import { capitalize } from '../utils/helpers'
 import sortBy from 'sort-by'
-import moment from 'moment'
 import { Nav, NavItem } from 'react-bootstrap'
-import EditIcon from 'react-icons/lib/fa/pencil'
-import DeleteIcon from 'react-icons/lib/fa/trash-o'
-import UpVoteIcon from 'react-icons/lib/fa/caret-up'
-import DownVoteIcon from 'react-icons/lib/fa/caret-down'
 
 class DefaultView extends Component {
   state = {
     addPostModalOpen: false,
-    editPostModalOpen: false,
   }
 
   componentDidMount() {
@@ -41,38 +34,12 @@ class DefaultView extends Component {
     this.setState(() => ({ addPostModalOpen: false }))
   }
 
-  openEditPostModal = (id) => {
-    this.setState(() => ({ editPostModalOpen: true }))
-  }
-
-  closeEditPostModal = (id) => {
-    this.setState(() => ({ editPostModalOpen: false }))
-  }
-
   onSort = () => {
     this.props.sortPosts()
   }
 
-  onUpVotePost = (id) => {
-    console.log('upVote_id', id)
-    this.props.votePost(id, 'upVote')
-  }
-
-  onDownVotePost = (id) => {
-    console.log('downVote_id', id)
-    this.props.votePost(id, 'downVote')
-  }
-
-  onDeletePost = (id) => {
-    console.log('delete_id', id)
-    this.props.deletePost(id)
-  }
-
   render() {
-    console.log('props',this.props)
-    console.log('state',this.state)
-    const { currentCategory, categories, posts, comments, sort } = this.props
-    const countComments = { '6ni6ok3ym7mf1p33lnez': 0, '8xf0y6ziyjabvozdd253nd': 2 }
+    const { currentCategory, categories, posts, sort } = this.props
 
     return (
       <div className='container'>
@@ -92,22 +59,7 @@ class DefaultView extends Component {
             <button onClick={()=>this.onSort()}>[ Sort by: {sort.posts} ]</button>
           </div>
           {posts.map((post) => (
-            <li key={post.id} className='list-item'>
-              <Link to={`/${post.category}/${post.id}`}><h4>{post.title}</h4></Link>
-              <div className='vote-edit-delete'>
-                <button onClick={()=>this.onUpVotePost(post.id)}><UpVoteIcon /></button>
-                {post.voteScore}
-                <button onClick={()=>this.onDownVotePost(post.id)}><DownVoteIcon /></button>
-                <button onClick={()=>this.openEditPostModal(post.id)}><EditIcon /></button>
-                <button onClick={()=>this.onDeletePost(post.id)}><DeleteIcon /></button>
-              </div>
-              <div className='info'>
-                {moment(parseInt(post.timestamp,10)).calendar()}{', '}
-                author: {post.author}{', '}
-                comment: {`${countComments[post.id]}`}{', '}
-                category: {post.category}
-              </div>
-            </li>
+            <PostContainer key={post.id} postId={post.id}/>
           ))}
         </ul>
       </div>
@@ -115,7 +67,7 @@ class DefaultView extends Component {
   }
 }
 
-function mapStateToProps ({ categories, posts, comments, sort }, { match : { params : { category }}}) {
+function mapStateToProps ({ categories, posts, sort }, { match : { params : { category }}}) {
   const currentCategory = category ? category : 'all'
   const showingPosts = posts
     .filter((post) => currentCategory === 'all' ? true : post.category === currentCategory)
@@ -124,7 +76,6 @@ function mapStateToProps ({ categories, posts, comments, sort }, { match : { par
     currentCategory: currentCategory,
     categories: categories,
     posts: showingPosts,
-    comments: comments,
     sort: sort
   }
 }
@@ -135,11 +86,6 @@ function mapDispatchToProps (dispatch) {
     getPostsByCategory: (category) => dispatch(getPostsByCategory(category)),
     getPosts: () => dispatch(getPosts()),
     addPost: (body) => dispatch(addPost(body)),
-    fetchPost: (id) => dispatch(fetchPost(id)),
-    votePost: (id, option) => dispatch(votePost(id, option)),
-    editPost: (id, body) => dispatch(editPost(id, body)),
-    deletePost: (id) => dispatch(deletePost(id)),
-    getComments: (parentId) => dispatch(getComments(parentId)),
     sortPosts: () => dispatch(sortPosts()),
   }
 }
--- src/components/PostView.js
+++ src/components/PostView.js
@@ -1,29 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { fetchPost, votePost, editPost, deletePost } from '../actions/post_actions'
-import { getComments, addComment, fetchComment, voteComment, editComment, deleteComment } from '../actions/comment_actions'
+import { getComments, addComment } from '../actions/comment_actions'
 import { sortComments } from '../actions/sort_actions'
+import PostContainer from './PostContainer'
+import CommentContainer from './CommentContainer'
 import sortBy from 'sort-by'
-import moment from 'moment'
 import HomeIcon from 'react-icons/lib/fa/home'
-import EditIcon from 'react-icons/lib/fa/pencil'
-import DeleteIcon from 'react-icons/lib/fa/trash-o'
-import UpVoteIcon from 'react-icons/lib/fa/caret-up'
-import DownVoteIcon from 'react-icons/lib/fa/caret-down'
 
 class PostView extends Component {
   state = {
     addPostModalOpen: false,
-    editPostModalOpen: false,
     addCommentModalOpen: false,
-    editCommentModalOpen: false,
   }
 
   componentDidMount() {
-    const currentPostId = this.props.match.params.id
-    this.props.fetchPost(currentPostId)
-    this.props.getComments(currentPostId)
+    this.props.getComments(this.props.currentPostId)
   }
 
   openAddPostModal = () => {
@@ -44,34 +36,11 @@ class PostView extends Component {
     this.setState(() => ({ addPostModalOpen: false }))
   }
 
-  openEditPostModal = (id) => {
-    this.setState(() => ({ editPostModalOpen: true }))
-  }
-
-  closeEditPostModal = (id) => {
-    this.setState(() => ({ editPostModalOpen: false }))
-  }
-
-  onUpVotePost = (id) => {
-    console.log('upVote_id', id)
-    this.props.votePost(id, 'upVote')
-  }
-
-  onDownVotePost = (id) => {
-    console.log('downVote_id', id)
-    this.props.votePost(id, 'downVote')
-  }
-
-  onDeletePost = (id) => {
-    console.log('delete_id', id)
-    this.props.deletePost(id)
-  }
-
   openAddCommentModal = () => {
     this.setState(() => ({ addCommentModalOpen: true }))
     this.props.addComment({
       id: '8tu4bsun805n8un48ve90',
-      parentId: this.props.post.id,
+      parentId: this.props.currentPostId,
       timestamp: 1469479767190,
       body: 'Comments. Are. Not. Cool.',
       author: 'thingone'
@@ -82,37 +51,12 @@ class PostView extends Component {
     this.setState(() => ({ addCommentModalOpen: false }))
   }
 
-  openEditCommentModal = (id) => {
-    this.setState(() => ({ editCommentModalOpen: true }))
-  }
-
-  closeEditCommentModal = (id) => {
-    this.setState(() => ({ editCommentModalOpen: false }))
-  }
-
   onSort = () => {
     this.props.sortComments()
   }
 
-  onUpVoteComment = (id) => {
-    console.log('upVote_id', id)
-    this.props.voteComment(id, 'upVote')
-  }
-
-  onDownVoteComment = (id) => {
-    console.log('downVote_id', id)
-    this.props.voteComment(id, 'downVote')
-  }
-
-  onDeleteComment = (id) => {
-    console.log('delete_id', id)
-    this.props.deleteComment(id)
-  }
-
   render() {
-    console.log('props', this.props)
-    console.log('state',this.state)
-    const { post, comments, sort } = this.props
+    const { currentPostId, comments, sort } = this.props
 
     return (
       <div className='container'>
@@ -120,21 +64,7 @@ class PostView extends Component {
         <Link to='/'><HomeIcon size={18}/></Link>
 
         <div className='post-detail'>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-          <div className='vote-edit-delete'>
-            <button onClick={()=>this.onUpVotePost(post.id)}><UpVoteIcon /></button>
-            {post.voteScore}
-            <button onClick={()=>this.onDownVotePost(post.id)}><DownVoteIcon /></button>
-            <button onClick={()=>this.openEditPostModal(post.id)}><EditIcon /></button>
-            <button onClick={()=>this.onDeletePost(post.id)}><DeleteIcon /></button>
-          </div>
-          <div className='info'>
-            {moment(parseInt(post.timestamp,10)).calendar()}{', '}
-            author: {post.author}{', '}
-            comments: {comments.length}{', '}
-            category: {post.category}
-          </div>
+          <PostContainer postId={currentPostId}/>
         </div>
 
         <ul className='list'>
@@ -146,20 +76,7 @@ class PostView extends Component {
             <button onClick={()=>this.onSort()}>[ Sort by: {sort.comments} ]</button>
           </div>
           {comments.map((comment) => (
-            <li key={comment.id} className='list-item'>
-              <div className='vote-edit-delete'>
-                <button onClick={()=>this.onUpVoteComment(comment.id)}><UpVoteIcon /></button>
-                {comment.voteScore}
-                <button onClick={()=>this.onDownVoteComment(comment.id)}><DownVoteIcon /></button>
-                <button onClick={()=>this.openEditCommentModal(comment.id)}><EditIcon /></button>
-                <button onClick={()=>this.onDeleteComment(comment.id)}><DeleteIcon /></button>
-              </div>
-              <div className='info'>
-                {moment(parseInt(comment.timestamp,10)).calendar()}{', '}
-                author: {comment.author}{', '}
-              </div>
-              {comment.body}
-            </li>
+            <CommentContainer key={comment.id} commentId={comment.id}/>
           ))}
         </ul>
       </div>
@@ -167,10 +84,11 @@ class PostView extends Component {
   }
 }
 
-function mapStateToProps ({ posts, comments, sort }) {
+function mapStateToProps ({ comments, sort }, { match : { params : { id }}}) {
+  const currentPostId = id
   const showingComments = comments.sort(sortBy(sort.comments))
   return {
-    post: posts,
+    currentPostId: currentPostId,
     comments: showingComments,
     sort: sort
   }
@@ -178,16 +96,8 @@ function mapStateToProps ({ posts, comments, sort }) {
 
 function mapDispatchToProps (dispatch) {
   return {
-    fetchPost: (id) => dispatch(fetchPost(id)),
-    votePost: (id, option) => dispatch(votePost(id, option)),
-    editPost: (id, body) => dispatch(editPost(id, body)),
-    deletePost: (id) => dispatch(deletePost(id)),
     getComments: (parentId) => dispatch(getComments(parentId)),
     addComment: (body) => dispatch(addComment(body)),
-    fetchComment: (id) => dispatch(fetchComment(id)),
-    voteComment: (id, option) => dispatch(voteComment(id, option)),
-    editComment: (id, body) => dispatch(editComment(id, body)),
-    deleteComment: (id) => dispatch(deleteComment(id)),
     sortComments: () => dispatch(sortComments()),
   }
 }
