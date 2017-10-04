8e58cdcc01d7e4b5d5a3edab03d3235fa3de733e
Claire Yan <silviaclaire@gmail.com>
2017-10-04 22:17:22 +0900

    Fix: Sort by descending order

--- src/reducers/sort_reducer.js
+++ src/reducers/sort_reducer.js
@@ -1,16 +1,16 @@
 import { SORT_POSTS, SORT_COMMENTS } from '../actions/sort_actions'
 
-const sortInitialState = { posts: 'voteScore', comments: 'voteScore' }
+const sortInitialState = { posts: '-voteScore', comments: '-voteScore' }
 
 export default function sortReducer (state = sortInitialState, action) {
   switch (action.type) {
     case SORT_POSTS :
       const newSortPosts =
-        state.posts === 'voteScore' ? 'timestamp' : 'voteScore'
+        state.posts === '-voteScore' ? '-timestamp' : '-voteScore'
       return { posts: newSortPosts, comments: state.comments }
     case SORT_COMMENTS :
       const newSortComments =
-        state.comments === 'voteScore' ? 'timestamp' : 'voteScore'
+        state.comments === '-voteScore' ? '-timestamp' : '-voteScore'
       return { posts: state.posts, comments: newSortComments }
     default :
       return state
